cb5b9e6c7499b262d63ff574a8da1ab75ded3a7d
Arisbel Hechavarría Rojas <pachidj87@gmail.com>
2017-12-15 16:40:41 -0500

    Ensure extra_def property is initialized

--- src/Traits/ObjectModelExtraTrait.php
+++ src/Traits/ObjectModelExtraTrait.php
@@ -44,7 +44,7 @@ trait ObjectModelExtraTrait {
 		parent::__construct($id, $id_lang, $id_shop, $translator);
 
 		$class_name = get_class($this);
-		if (!isset(ObjectModel::$loaded_classes[$class_name])) {
+		if (isset(ObjectModel::$loaded_classes[$class_name]) && $this->def_extra === null) {
 			$this->def_extra = ObjectModel::getDefinitionExtra($class_name);
 			if (!Validate::isTableOrIdentifier($this->def_extra['primary']) || !Validate::isTableOrIdentifier($this->def_extra['table'])) {
 				throw new PrestaShopException('Identifier or table format not valid for class '.$class_name.' in extra fields');
